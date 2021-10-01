b5ef8180371d6be4931b4804c0921c4c5673682d
ismaelsky <ismael.sky1@gmail.com>
2021-10-01 14:17:41 -0300

    Lista de Alunos

---
+++ script.js
@@ -0,0 +1,77 @@
+let alunos = [
+// {
+//     "nome":"Pedro",
+//     "idade": 17,
+//     "sexo":"masculino",
+//     "turno":"matutino",
+// },
+
+// {
+//     "nome":"Tiago",
+//     "idade": 17,
+//     "sexo":"masculino",
+//     "turno":"matutino",
+// },
+       
+// {
+//     "nome":"Ana",
+//     "idade": 17,
+//     "sexo":"feminino",
+//     "turno":"matutino",
+// },
+       
+// {
+//     "nome":"Maria",
+//     "idade": 17,
+//     "sexo":"feminino",
+//     "turno":"matutino",
+// },
+       
+// {
+//     "nome":"João",
+//     "idade": 17,
+//     "sexo":"masculino",
+//     "turno":"vespertino",
+// },
+        
+// {
+//     "nome":"Lucas",
+//     "idade": 17,
+//     "sexo":"masculino",
+//     "turno":"vespertino",
+// },
+
+// {
+//     "nome":"juliana",
+//     "idade": 17,
+//     "sexo":"feminino",
+//     "turno":"vespertino"
+// },
+
+// {
+//     "nome":"Paula",
+//     "idade": 17,
+//     "sexo":"feminino",
+//     "turno":"vespertino"
+// },
+     
+]
+
+function listarAlunos(params){
+    console.log(alunos)
+    return
+}
+
+function adicionarAlunos(params){
+    alunos.push({
+        "nome":"Pedro",
+        "idade": 17,
+        "sexo":"masculino",
+        "turno":"matutino",
+    })
+}
+
+function removerAlunos(params){
+    alunos.splice( "" );
+}
+
